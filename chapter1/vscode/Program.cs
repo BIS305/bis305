bdaeabaaf2cfa2b9222fe99b7c5aa0ffb0adbbf1
m2web <m2web@yahoo.com>
2020-08-14 17:09:08 -0400

    Commented out lines for instruction

--- chapter1/vscode/Program.cs
+++ chapter1/vscode/Program.cs
@@ -7,12 +7,12 @@ namespace vscode
         static void Main(string[] args)
         {
             Console.WriteLine("{0}", 1234);
-            Console.WriteLine("{0, 5}", 1234);
-            Console.WriteLine("{0, 8}", 1234);
-            Console.WriteLine("{0} {1}", "Mark", "McFadden");
-            Console.WriteLine("{0, -5}{1}", "Mark", "McFadden");
-            Console.WriteLine("{0, -6}{1}", "Mark", "McFadden");
-            Console.WriteLine("{0, -7}{1}", "Mark", "McFadden");
+            // Console.WriteLine("{0, 5}", 1234);
+            // Console.WriteLine("{0, 8}", 1234);
+            // Console.WriteLine("{0} {1}", "Mark", "McFadden");
+            // Console.WriteLine("{0, -5}{1}", "Mark", "McFadden");
+            // Console.WriteLine("{0, -6}{1}", "Mark", "McFadden");
+            // Console.WriteLine("{0, -7}{1}", "Mark", "McFadden");
         }
     }
 }
