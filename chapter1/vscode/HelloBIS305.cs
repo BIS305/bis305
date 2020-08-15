6273ebe0782cf1907c743dd345fb4f6e744d4178
m2web <m2web@yahoo.com>
2020-08-15 13:49:13 -0400

    Renamed class name in code to mathc file name.

--- chapter1/vscode/HelloBIS305.cs
+++ chapter1/vscode/HelloBIS305.cs
@@ -1,6 +1,6 @@
 //Filename HelloBIS305.cs
 using static System.Console;
-class Hello
+class HelloBIS305
 {
    /* This program demonstrates the use of the WriteLine()
    method to display the message Hello, world! */
