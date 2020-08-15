560e0854daef055b4510462bc49b5cfbdda8388d
m2web <m2web@yahoo.com>
2020-08-15 11:28:59 -0400

    Chapter 1 updates

---
+++ chapter1/vscode/HelloBIS305.cs
@@ -0,0 +1,12 @@
+//Filename HelloBIS305.cs
+using static System.Console;
+class Hello
+{
+   /* This program demonstrates the use of the WriteLine()
+   method to display the message Hello, world! */
+   static void Main(string[] args)
+   {
+      WriteLine("Hello, BIS 305!");
+   }
+}
+
