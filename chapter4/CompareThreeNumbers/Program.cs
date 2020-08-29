d89dca44fd9f73069a5de45bb228bd817d4bf85e
Mark McFadden <m2web@yahoo.com>
2020-08-29 12:23:43 -0400

    Updated if-else logic to use loop and different syntax convention

--- chapter4/CompareThreeNumbers/Program.cs
+++ chapter4/CompareThreeNumbers/Program.cs
@@ -7,33 +7,65 @@ namespace CompareThreeNumbers
     {
         static void Main(string[] args)
         {
-            string numberString;
-            int num1, num2, num3;
-            Write("Enter an integer ");
-            numberString = ReadLine();
-            num1 = Convert.ToInt32(numberString);
-            Write("Enter an integer ");
-            numberString = ReadLine();
-            num2 = Convert.ToInt32(numberString);
-            Write("Enter an integer ");
-            numberString = ReadLine();
-            num3 = Convert.ToInt32(numberString);
-            if (num1 == num2)
-                if (num1 == num3)
-                    WriteLine("All three numbers are equal");
-                else
-                    WriteLine("First two are equal");
-            else
-               if (num1 == num3)
-                WriteLine("First and last are equal");
-            else
-                  if (num2 == num3)
-                WriteLine("Last two are equal");
-            else
-                WriteLine("No two numbers are equal");
+            int numberOfOutcomes = 5;
 
-            Write("[Press any key to end]");
-            ReadKey();
+            for (int i = 0; i < numberOfOutcomes; i++)
+            {
+                string numberString;
+                int num1, num2, num3;
+                Write("Enter an integer ");
+                numberString = ReadLine();
+                num1 = Convert.ToInt32(numberString);
+                Write("Enter an integer ");
+                numberString = ReadLine();
+                num2 = Convert.ToInt32(numberString);
+                Write("Enter an integer ");
+                numberString = ReadLine();
+                num3 = Convert.ToInt32(numberString);
+
+                if (num1 == num2)
+                    if (num1 == num3)
+                        WriteLine("All three numbers are equal");
+                    else
+                        WriteLine("First two are equal");
+                else
+                    if (num1 == num3)
+                    WriteLine("First and last are equal");
+                else
+                    if (num2 == num3)
+                    WriteLine("Last two are equal");
+                else
+                    WriteLine("No two numbers are equal");
+
+                //convention that I like
+                //if (num1 == num2)
+                //{
+                //    if (num1 == num3)
+                //    {
+                //        WriteLine("All three numbers are equal");
+                //    }
+                //    else
+                //    {
+                //        WriteLine("First two are equal");
+                //    }
+                //}
+                //else if (num1 == num3)
+                //{
+                //    WriteLine("First and last are equal");
+                //}
+                //else if(num2 == num3)
+                //{
+                //    WriteLine("Last two are equal");
+                //}
+                //else
+                //{
+                //    WriteLine("No two numbers are equal");
+                //}
+
+                WriteLine("[Press any key to continue]");
+                ReadKey();
+                WriteLine("");
+            }
         }
     }
 }
