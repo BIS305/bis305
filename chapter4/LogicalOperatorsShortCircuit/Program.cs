8e27511d5df2018f86885a073a3065b7e478e0bb
Mark McFadden <m2web@yahoo.com>
2020-08-29 10:20:33 -0400

    Demo to show logical opererators eval process

---
+++ chapter4/LogicalOperatorsShortCircuit/Program.cs
@@ -0,0 +1,42 @@
+﻿//adapted from https://www.c-sharpcorner.com/article/short-circuit-evaluation-in-c-sharp/
+
+using static System.Console;
+
+namespace LogicalOperatorsShortCircuit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool Condition1() { 
+                return false; 
+            }
+
+            bool Condition2() { 
+                return true; 
+            }
+
+            if (Condition1() & Condition2())
+            {
+                //inside code will not execute
+                //both operands evaluated
+            }
+            if (Condition2() | Condition1())
+            {
+                //inside code will execute
+                //both operands evaluated
+            }
+
+            if (Condition1() && Condition2())
+            {
+                //inside code will not execute
+                //Condition2 not evaluated
+            }
+            if (Condition2() || Condition1())
+            {
+                //inside code will execute
+                //Condition2 not evaluated
+            }
+        }
+    }
+}
