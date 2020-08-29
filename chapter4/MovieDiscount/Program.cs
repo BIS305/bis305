34263da45d85cd34a6e493240ec502e7394a4720
Mark McFadden <m2web@yahoo.com>
2020-08-29 13:50:49 -0400

    Added MovieDiscount for use of AND and OR together

---
+++ chapter4/MovieDiscount/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static System.Console;
+
+namespace MovieDiscount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int age = 10;
+			char rating = 'R';
+			const int CHILD_AGE = 12;
+			const int SENIOR_AGE = 65;
+			WriteLine("When age is {0} and rating is {1}", age, rating);
+			if ((age <= CHILD_AGE || age >= SENIOR_AGE) && rating == 'G')
+				WriteLine("Discount applies");
+			else
+				WriteLine("Full price");
+
+			Write("[Press any key to continue]");
+			ReadKey();
+		}
+    }
+}
