7ff4c848bb2fbeb910203b6dfb764e2177f2ad3c
Mark McFadden <m2web@yahoo.com>
2020-09-13 12:29:31 +0000

    Updated from GitHub in codespace

--- GitHubCodespace/4Codespace/Program.cs
+++ GitHubCodespace/4Codespace/Program.cs
@@ -7,6 +7,9 @@ namespace _4Codespace
         static void Main(string[] args)
         {
             Console.WriteLine("Hello GitHub Codespaces!");
+            Console.Write("What is your name? ");
+            var name = Console.ReadLine();
+            Console.WriteLine("Hello {0}! Welcome to my codespace!", name);
         }
     }
 }
