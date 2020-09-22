3052dca327dd3d3c7598b46187214008742373a1
Mark McFadden <m2web@yahoo.com>
2020-09-22 05:57:09 -0400

    Chapter 7 Using Methods

---
+++ chapter7/ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Console;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            ShowWelcomeMessage();
+            WriteLine("Hello");
+        }
+        private static void ShowWelcomeMessage()
+        {
+            WriteLine("Welcome.");
+            WriteLine("Have fun!");
+            WriteLine("Enjoy the program!");
+        }
+    }
+}
